8bfc695f3b8cf9318d3efe7597783137f37ca2c0
sylhare <sylhare@outlook.com>
2020-11-11 22:44:45 -0500

    Add df creation from csv

--- scripts/generate_data.sh
+++ scripts/generate_data.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-while true; do docker stats --no-stream >> data.txt; done
+#while true; do docker stats --no-stream > dockerstats; tail -n +2 dockerstats >> data.txt; sleep 5; done
+while true
+do docker stats --no-stream --format "table {{.Name}};{{.CPUPerc}};{{.MemPerc}};{{.MemUsage}};{{.NetIO}};{{.BlockIO}};{{.PIDs}}" > dockerstats
+tail -n +2 dockerstats | awk -v date=";$(date +%T)" '{print $0, date}' >> data.csv
+sleep 5
+done
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,9 @@
+import os
 import unittest
 
+import pandas as pd
+
+from src import ROOT_PATH
 from src.app import hello
 
 
@@ -10,6 +14,16 @@ class ParserTest(unittest.TestCase):
 
     def test_hello_world(self):
         self.assertEqual("Hello World!", hello())
+
+    @staticmethod
+    def test_open_file():
+        """{{.Name}};{{.CPUPerc}};{{.MemPerc}};{{.MemUsage}};{{.NetIO}};{{.BlockIO}};{{.PIDs}}"""
+        header_list = ["Name", "CPU %", "MEM %", "MEM Usage", "NET IO", "BLOCK IO", "PID", "DATE"]
+        path = os.path.join(ROOT_PATH, "tests", "resources", "data.csv")
+        print(path)
+        df = pd.read_csv(path, delimiter=";", names=header_list)
+        print(df)
+
 
     def tearDown(self):
         print("Tear Down - After each Test\n")
