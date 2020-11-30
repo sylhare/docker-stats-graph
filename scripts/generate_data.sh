5dbba81b0fb3eeb31313f4d3ef0cb511da8ec4ee
sylhare <sylhare@outlook.com>
2020-11-30 14:06:57 -0500

    Update script with time

--- scripts/generate_data.sh
+++ scripts/generate_data.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-#while true; do docker stats --no-stream > dockerstats; tail -n +2 dockerstats >> data.txt; sleep 5; done
-while true
+# Using linux date
+# For mac - brew install coreutils ; echo "alias date=gdate" >> ~/.bash_profile
+runtime="30 minutes"
+endtime=$(date -ud "$runtime" +%s)
+
+# while true
+while [[ $(date -u +%s) -le ${endtime} ]]
 do docker stats --no-stream --format "table {{.Name}};{{.CPUPerc}};{{.MemPerc}};{{.MemUsage}};{{.NetIO}};{{.BlockIO}};{{.PIDs}}" > dockerstats
 tail -n +2 dockerstats | awk -v date=";$(date +%T)" '{print $0, date}' >> data.csv
 sleep 5
